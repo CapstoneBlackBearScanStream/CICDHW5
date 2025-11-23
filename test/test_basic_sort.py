4639d7e97167b3b298f5b23814c80993068ec34c
l-anderson-e <157403432+l-anderson-e@users.noreply.github.com>
2025-11-23 16:55:17 -0500

    update formatting to pass flake8 linting

--- test/test_basic_sort.py
+++ test/test_basic_sort.py
@@ -18,7 +18,7 @@
 
 import pytest
 import numpy as np
-from basic_sort_BBScanStream397.int_sort import bubble, quick, insertion
+from basic_sort_BBScanStream397.int_sort import bubble  # , quick, insertion
 
 
 def is_sorted(values):
